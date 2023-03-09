65fc8cb33c3317ec38880c43400a91ede49da61c
Abdul Salam <abdulsalam.ksef18@iba-suk.edu.pk>
2023-03-10 01:37:43 +0500

    update code

--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,58 @@
-import { Html, Head, Main, NextScript } from 'next/document'
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const originalRenderPage = ctx.renderPage
+
+    // Run the React rendering logic synchronously
+    ctx.renderPage = () =>
+      originalRenderPage({
+        // Useful for wrapping the whole react tree
+        enhanceApp: (App) => App,
+        // Useful for wrapping in a per-page basis
+        enhanceComponent: (Component) => Component,
+      })
+
+    // Run the parent `getInitialProps`, it now includes the custom `renderPage`
+    const initialProps = await Document.getInitialProps(ctx)
+
+    return initialProps
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+        <meta charset="utf-8" />
+<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+<link href="css/bootstrap.min.css" rel="stylesheet" />
+<link href="css/font-awesome-all.min.css" rel="stylesheet" />
+<link rel="stylesheet" href="css/jquery.mCustomScrollbar.css"/>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fancyapps/ui@4.0/dist/fancybox.css" />
+<link href="css/custom.css" rel="stylesheet" type="text/css"/>
+
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>  
+<script src="js/bootstrap.bundle.min.js"></script>
+<script src="js/font-awesome-all.min.js"></script> 
+        </Head >
+        <body>
+          <Main />
+          <NextScript />
+          
+<script src="https://cdn.jsdelivr.net/npm/@fancyapps/ui@4.0/dist/fancybox.umd.js"></script>
+<script src="js/custom.js"></script>
+<script src="js/jquery.mCustomScrollbar.concat.min.js"></script>
+
+
+
+        </body>
+      </Html>
+    )
+  }
 }
+
+export default MyDocument
